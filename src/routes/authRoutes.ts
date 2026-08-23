ef75f6b83583aa1b5be648e85cd140cae0919426
AlissonGomezG <allissongomezgonzalez@gmail.com>
2026-08-22 23:04:40 -0600

    Se agrego la funcion para cambiar la contraseña cuando el usuario esta dentro del sistema.

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -9,7 +9,8 @@ import {
   verifyLoginCode,
   resendLoginCode,
   resendResetCode,
-  editProfile,} from "../controllers/authController";
+  editProfile,
+changePassword} from "../controllers/authController";
 import { validateBody } from "../middleware/validations";
 import { registerUserSchema } from "../db/schemas/userSchema";
 import { authenticate } from "../middleware/authenticate";
@@ -26,5 +27,5 @@ router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.post("/resend-reset-code", resendResetCode);
 router.patch("/edit-profile", authenticate, editProfile);
-
+router.patch("/change-password", authenticate, changePassword);
 export default router;
